edd7d09913d4ea36cadec600f4bd21c6cf510b43
Hunter Dyar <bloop@hdyar.com>
2021-05-06 15:26:54 -0400

    negligable comments update

--- EventTransition.cs
+++ EventTransition.cs
@@ -6,7 +6,8 @@ namespace Bloops.StateMachine
 	/// <summary>
 	/// An event is just a transition that doesnt need conditions, and instantly triggers the stateMachine it is attached to (that is configured on state machine init).
 	/// You may not need this, like if you are triggering the state machine in update.
-	/// I wanted to keep my UIEvents - like closing a dialogue box - tidy, so I made this for a "returnToGameplay" transition.
+	/// I want to keep my UIEvents - like closing a dialogue box - tidy, so I made this for a "returnToGameplay" transition.
+	/// Its probably a bad idea to have a state machine that uses both update-loop-triggers and event-style-triggers.
 	/// </summary>
 	
 	[CreateAssetMenu(fileName = "Event Transition", menuName = "Bloops/State/Event Transition", order = 4)]
